1b287432571d912c078b3d623cc0f242062859f4
Robrt <raaguilarm@gmail.com>
2019-11-28 15:52:16 -0500

    Casi primera seccion

--- main.js
+++ main.js
@@ -1,19 +1,62 @@
+//Funcion para limpiar los imputs molaridad
+function limpiar1(){
+    document.getElementById('soluto').value = "";
+    document.getElementById('solucion').value = "";
+    console.log('Limpiar ready');
+}
+//Funcion de molaridad
+function molaridad(){
+    var soluto = parseFloat(document.getElementById('soluto').value);
+    var volM = parseFloat(document.getElementById('solucion').value);
+    var resMola = (soluto/volM);
+    console.log("Molaridad ready");
+    if(isNaN(resMola)){
+        document.getElementById('resp').innerText = "Ingresa valores";
+    }else{
+        document.getElementById('resp').innerText = "Molaridad "+resMola.toFixed(2);
+    };
+    limpiar1();
+}
+//Limpiar los imput de molalidad
+function limpiar2(){
+    document.getElementById('solutoMola').value = "";
+    document.getElementById('entryKg').value = "";
+    console.log('Limpiar ready');
+}
+//Funcion de molalidad
+function molalidad(){
+    var solutoMolali = parseFloat(document.getElementById('solutoMola').value);
+    var varKg = parseFloat(document.getElementById('entryKg').value);
+    var resMolali = (solutoMolali/varKg);
+    console.log(resMolali);
+    console.log("Molalidad ready");
+    if(isNaN(resMolali)){
+        document.getElementById('respMolalidad').innerText = "Ingresa valores ";
+    }else{
+        document.getElementById('respMolalidad').innerText = "Molalidad "+resMolali.toFixed(2);
+    };
+    limpiar2();
+}
 
-  function mol(){
-      var soluto = parseFloat(document.getElementById('soluto').value);
-      var solucion = parseFloat(document.getElementById('solucion').value);
-      var resMola = (soluto/solucion) * 100;
-      console.log(resMola);
-      document.getElementById('resp').innerText = "Molaridad "+resMola.toFixed(2);
-  }
+function masaMasa(){
+    var soluto1 = parseFloat(document.getElementById('solutoMM').value);
+    var solucion1 = parseFloat(document.getElementById('solucionMM').value);
+    var solucionSuma = soluto1 + solucion1;
+    var resMMsuma = (soluto1/solucionSuma) * 100;
+    console.log(resMMsuma);
+    console.log("Molalidad ready");
+    if(isNaN(resMolali)){
+        document.getElementById('respMolalidad').innerText = "Ingresa valores ";
+    }else{
+        document.getElementById('respMolalidad').innerText = "Molalidad "+resMMsuma.toFixed(2);
+    };
+    limpiar2();
+}
+
+
+
 
 $(document).ready(function() {
-  function limpiar2(){
-      document.getElementById('soluto').value = "";
-      document.getElementById('solucion').value = "";
-  }
-
-  $('#bt-send').on('click',limpiar2,mol);
 
   $( function() {
       $( "#tabs-1" ).tabs();
